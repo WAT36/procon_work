460534ddfb6414a025d8c34846ef25039963c976
T.Wakasugi <motohari.xanadu@gmail.com>
2019-06-01 18:16:03 +0900

    ABC047-C Java

---
+++ procon_java/src/Atcoder/abc_past/C_ABC047_1DRevarsi.java
@@ -0,0 +1,34 @@
+package Atcoder.abc_past;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class C_ABC047_1DRevarsi {
+
+	public static void main(String args[]) {
+
+		Scanner sc = new Scanner(System.in);
+
+		String S = sc.nextLine();
+
+		char c = S.charAt(0);
+		int N = S.length();
+		List<Integer> bw = new ArrayList<>();
+		int temp=0;
+		for(int i=0;i<N;i++) {
+			if(c == S.charAt(i)) {
+				temp++;
+			}else {
+				bw.add(temp);
+				temp=1;
+				c = S.charAt(i);
+			}
+		}
+		bw.add(temp);
+
+		System.out.println(bw.toString());
+		System.out.println(bw.size()-1);
+	}
+
+}
