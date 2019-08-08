f28664d747128016ea56988d80393a1f8db4ce63
T.Wakasugi <motohari.xanadu@gmail.com>
2019-08-08 22:31:57 +0900

    ABC128-A  台湾旅行の準備前にちょちょいと

---
+++ procon_java/src/Atcoder/abc_past/A_128_Applepie.java
@@ -0,0 +1,15 @@
+package Atcoder.abc_past;
+
+import java.util.Scanner;
+
+public class A_128_Applepie {
+
+	public static void main(String args[]) {
+
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int P = sc.nextInt();
+		System.out.println((3*A + P)/2);
+	}
+
+}
