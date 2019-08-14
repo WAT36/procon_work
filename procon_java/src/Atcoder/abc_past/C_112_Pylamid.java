b99ca8ac6c9a1c35f661c0f974db327b44eadd98
T.Wakasugi <motohari.xanadu@gmail.com>
2019-08-15 01:18:48 +0900

    ABC112-C  少し解説見たけどクリア　これも全探索問だった

--- procon_java/src/Atcoder/abc_past/C_112_Pylamid.java
+++ procon_java/src/Atcoder/abc_past/C_112_Pylamid.java
@@ -1,5 +1,6 @@
 package Atcoder.abc_past;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class C_112_Pylamid {
@@ -8,36 +9,45 @@ public class C_112_Pylamid {
 
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-
 		int[][] xyh = new int[N][3];
-		for (int i = 0; i < N; i++) {
-			xyh[i] = new int[] { sc.nextInt(), sc.nextInt(), sc.nextInt() };
+
+		for(int i=0;i<N;i++) {
+			xyh[i][0] = sc.nextInt();
+			xyh[i][1] = sc.nextInt();
+			xyh[i][2] = sc.nextInt();
 		}
 
-		int p;
-		for (p = 0; p < N; p++) {
-			if (xyh[p][2] > 0) {
-				break;
-			}
+		Arrays.sort(xyh, (a,b)->Integer.compare(b[2], a[2]));
+		if(xyh[0][2] == 0) {
+			System.out.println("7 7 1");
+			return;
 		}
 
-		for (int i = 0; i < 100; i++) {
-			for (int j = 0; j < 100; j++) {
-				next: {
-					int H = xyh[p][2] - Math.abs(xyh[p][0] - i) - Math.abs(xyh[p][1] - j);
-
-					for (int k = 0; k < N; k++) {
-						int hk = xyh[k][2] + Math.abs(xyh[k][0] - i) + Math.abs(xyh[k][1] - j);
-
-						if (H != hk && xyh[k][2] > 0) {
-							break next;
-						}
-						else if(H > hk && xyh[k][2] <= 0) {
-							break next;
+		boolean flag = true;
+		for(int i=0;i<=100;i++) {
+			for(int j=0;j<=100;j++) {
+				int h = 0;
+				for(int k=0;k<N;k++) {
+					if(h == 0) {
+						if(xyh[k][2] == 0) {
+							continue;
+						}else {
+							h = xyh[k][2] + Math.abs(xyh[k][0] - i) + Math.abs(xyh[k][1] - j);
 						}
 					}
-					System.out.println(i+" "+j+" "+H);
+
+					int hk = Math.max(0, h - Math.abs(xyh[k][0] - i) - Math.abs(xyh[k][1] - j));
+					if(xyh[k][2] != hk) {
+						flag = false;
+						break;
+					}
+				}
+
+				if(flag) {
+					System.out.println(i + " " + j + " " + h);
 					return;
+				}else {
+					flag = true;
 				}
 			}
 		}
