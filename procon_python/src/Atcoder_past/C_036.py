d63eccfbfe09956586545eebace2361448f743ee
T.Wakasugi <motohari.xanadu@gmail.com>
2019-08-29 22:43:32 +0900

    ABC036-C  解説見た　indexOfよりmap使った方が性能いいらしい　座標圧縮というテクでプロコンでは頻出だそうだ

---
+++ procon_python/src/Atcoder_past/C_036.py
@@ -0,0 +1,14 @@
+N=int(input())
+a=[]
+for i in range(N):
+    a.append(int(input()))
+a_set = list(set(a))
+a_set.sort()
+
+d = {}
+for i,v in enumerate(a_set):
+    d[v] = i
+
+
+for i in range(N):
+    print(d[a[i]])
