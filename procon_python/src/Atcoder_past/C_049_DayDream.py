64ee2ba686e486d761538b295927ce5f8971e2a4
T.Wakasugi <motohari.xanadu@gmail.com>
2019-09-02 22:03:07 +0900

    ABC049-C  python 文字列・リストを逆にする方法は？

---
+++ procon_python/src/Atcoder_past/C_049_DayDream.py
@@ -0,0 +1,19 @@
+S=''.join(list(reversed(str(input()))))
+words=["maerd","remaerd","esare","resare"]
+flag = False
+while(True):
+    if(len(S) == 0):
+        print("YES")
+        break
+    elif(S[0:len(words[0])] == words[0]):
+        S = S[len(words[0]):]
+    elif(S[0:len(words[1])] == words[1]):
+        S = S[len(words[1]):]
+    elif(S[0:len(words[2])] == words[2]):
+        S = S[len(words[2]):]
+    elif(S[0:len(words[3])] == words[3]):
+        S = S[len(words[3]):]
+    else:
+        print("NO")
+        break
+
