ba10604947261eaad8cdfbe8c52417e7583042f6
T.Wakasugi <motohari.xanadu@gmail.com>
2019-09-07 00:24:37 +0900

    ABC029-C  解説と違うが　3進数作って文字列作らせた

---
+++ procon_python/src/Atcoder_past/C_029.py
@@ -0,0 +1,13 @@
+n=int(input())
+w=["a","b","c"]
+full_num=3**n
+for i in range(full_num):
+    pass_i=[]
+    temp_i = i
+    temp_n = n
+    password=""
+    while(temp_i != 0 or temp_n != 0):
+        password = w[temp_i%3] + password
+        temp_i = temp_i//3
+        temp_n -= 1
+    print(password)
