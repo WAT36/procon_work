44e1af7035765953291ac36cc0f1f05068e8a3ff
T.Wakasugi <motohari.xanadu@gmail.com>
2019-09-21 13:45:21 +0900

    ABC001-C  Pythonでの四捨五入ってすごくやりづらいのね

---
+++ procon_python/src/Atcoder_past/C_001.py
@@ -0,0 +1,7 @@
+import bisect
+from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_EVEN
+deg,dis=map(int,input().split())
+direct=["N","NNE","NE","ENE","E","ESE","SE","SSE","S","SSW","SW","WSW","W","WNW","NW","NNW","N"]
+speed=[0,3,16,34,55,80,108,139,172,208,245,285,327]
+wind_power=bisect.bisect(speed,Decimal(dis/6).quantize(Decimal('0'), rounding=ROUND_HALF_UP))-1
+print((direct[(deg*10+1125)//2250] if wind_power != 0 else "C")+" "+str(wind_power))
