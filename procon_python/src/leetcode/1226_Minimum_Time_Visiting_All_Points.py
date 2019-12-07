3e8dfb849623fe303e62733d4fcb85eded380895
T.Wakasugi <motohari.xanadu@gmail.com>
2019-12-07 15:17:57 +0900

    LeetCode#1226  最適経路とは言いながら　訪れる順番は点の通りなのね・・

---
+++ procon_python/src/leetcode/1226_Minimum_Time_Visiting_All_Points.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        ans=0
+        now=points[0]
+        for i in range(1,len(points)):
+            next=points[i]
+            diff_x=abs(next[0]-now[0])
+            diff_y=abs(next[1]-now[1])
+            ans+=min(diff_x,diff_y) + abs(diff_y-diff_x)
+            now=next
+        return ans
