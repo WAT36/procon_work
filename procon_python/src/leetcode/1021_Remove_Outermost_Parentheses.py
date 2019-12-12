9f6c90b8db37a3842879c3f037bc8e2f98276aee
T.Wakasugi <motohari.xanadu@gmail.com>
2019-12-12 22:50:57 +0900

    Leetcode#1021  ここもリストのjoinが役立った

---
+++ procon_python/src/leetcode/1021_Remove_Outermost_Parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def removeOuterParentheses(self, S: str) -> str:
+        level=0
+        index=[]
+        slist=list(S)
+        for i in range(len(S)):
+            if(S[i]=="("):
+                if(level==0):
+                    index.add(i)
+                level+=1
+            else:
+                level-=1
+                if(level==0):
+                    index.add(i)
+
+        for i in range(len(index)):
+            slist[index[i]]=''
+
+        return ''.join(slist)
