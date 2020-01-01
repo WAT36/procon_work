598950f89ef2ef0084dcf4b11e8245ecb5233245
T.Wakasugi <motohari.xanadu@gmail.com>
2020-01-01 23:51:52 +0900

    Leetcode#5295

---
+++ procon_python/src/leetcode/5295_Find_N_Unique_Integers_Sum_up_to_Zero.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        ans=[]
+        if(n%2!=0):
+            ans.append(0)
+            n-=1
+
+        for i in range(1,n+1,2):
+            ans.append(i)
+            ans.append(-1*i)
+
+        return ans
+
