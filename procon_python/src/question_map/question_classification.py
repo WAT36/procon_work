86cc25a878d68e201a7dfbf43579a189d9910442
T.Wakasugi <motohari.xanadu@gmail.com>
2020-04-13 00:02:35 +0900

    緑問は20分、水問は30分くらいか

--- procon_python/src/question_map/question_classification.py
+++ procon_python/src/question_map/question_classification.py
@@ -4,7 +4,8 @@ classification=[
     "動的計画法",
     "スタック",
     "二分探索",
-    ""
+    "フィボナッチ数列",
+    "ユークリッドの互除法"
     ]
 
 question={}
@@ -14,5 +15,6 @@ question["ARC-002-C"]=["動的計画法"]
 question["ARC-002-C"]=["動的計画法"]
 question["AOJ-ALDS-1-3-D"]=["スタック"]
 question["AOJ-ALDS-1-4-D"]=["二分探索"]
+question["ARC-051-B"]=["フィボナッチ数列","ユークリッドの互除法"]
 
 
