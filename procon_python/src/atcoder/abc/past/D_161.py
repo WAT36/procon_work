fe230a0b4996a776bcc88f432e62f82bf0fbb6e5
T.Wakasugi <motohari.xanadu@gmail.com>
2020-04-15 08:23:33 +0900

    ABC161-D  緑問　前できなかった問題　これも忘れた頃にやってみたい

---
+++ procon_python/src/atcoder/abc/past/D_161.py
@@ -0,0 +1,23 @@
+from collections import deque
+k=int(input())
+cnt=0
+ans=0
+q=deque()
+q.extend([1,2,3,4,5,6,7,8,9])
+
+while(cnt<k):
+    ans=q.popleft()
+    cnt+=1
+    print(ans,cnt)
+    one_digit=ans%10
+    if(one_digit==0):
+        q.append(int(str(ans) + '0'))
+        q.append(int(str(ans) + '1'))
+    elif(one_digit==9):
+        q.append(int(str(ans) + '8'))
+        q.append(int(str(ans) + '9'))
+    else:
+        q.append(int(str(ans) + str(one_digit-1)))
+        q.append(int(str(ans) + str(one_digit)))
+        q.append(int(str(ans) + str(one_digit+1)))
+print(ans)
