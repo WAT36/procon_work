1fe5d54df0b85e260d31faa6789f9ba1ca8c93cd
T.Wakasugi <motohari.xanadu@gmail.com>
2021-02-28 19:18:15 +0900

    第一回日本最強プログラマー学生選手権予選 B  ずっと解けなかった緑門が今なぜか溶けるようになっている・・

---
+++ procon_python/src/atcoder/corporate/B_ProgrammingKing1_qual.py
@@ -0,0 +1,18 @@
+n,k=map(int,input().split())
+a=list(map(int,input().split()))
+l=[0 for _ in range(len(a))]
+r=[0 for _ in range(len(a))]
+MOD=(10**9)+7
+
+for i in range(len(a)):
+    for j in range(len(a)):
+        if(j<i and a[j]<a[i]):
+            l[i]+=1
+        elif(i<j and a[i]>a[j]):
+            r[i]+=1
+
+print(l)
+print(r)
+ans=((k*(1+k)*sum(r)//2) + (k*(k-1)*sum(l)//2))%MOD
+print(ans)
+
