3571ce4ea5e16555754d8242c9b37fde9dc78899
T.Wakasugi <motohari.xanadu@gmail.com>
2021-03-06 14:46:23 +0900

    今日も緑門できなかったので　Leetcode#1470

---
+++ procon_python/src/leetcode/1470_Shuffle_the_Array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        x=nums[:n]
+        y=nums[n:]
+        ans=[]
+        for i in range(n):
+            ans.append(x[i])
+            ans.append(y[i])
+        return ans
