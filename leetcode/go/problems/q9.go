3c73cdce08f0ef759a3cd38f6a27e21ee63f1951
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-05-25 16:23:34 +0900

    Q9

---
+++ leetcode/go/problems/q9.go
@@ -0,0 +1,18 @@
+package problems
+
+import (
+	"strconv"
+)
+
+func Q9(x int) bool {
+	if x < 0 {
+		return false
+	}
+	s := strconv.Itoa(x)
+	for i := 0; i < len(s)/2; i++ {
+		if s[i] != s[len(s)-1-i] {
+			return false
+		}
+	}
+	return true
+}
