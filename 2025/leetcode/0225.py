e794fcb7d24cdf939f930b8373ca021f8759eb4b
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-07-29 07:54:50 +0900

    0225  昨日は残業・・・

---
+++ 2025/leetcode/0225.py
@@ -0,0 +1,25 @@
+class MyStack:
+    stack = None
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        return self.stack.pop(-1)
+
+    def top(self) -> int:
+        return self.stack[-1] if len(self.stack) > 0 else None
+
+    def empty(self) -> bool:
+        return len(self.stack) == 0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
