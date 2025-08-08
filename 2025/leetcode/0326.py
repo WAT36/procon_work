43513f95f987183383be938777bd7ff7b6aebc69
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-08-09 00:20:30 +0900

    0326

---
+++ 2025/leetcode/0326.py
@@ -0,0 +1,7 @@
+import math
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        x = round(math.log(n,3))
+        return 3 ** x == n
