c68faa172bdcc4c61a4d914acf3e5406f095f6d9
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-08-15 22:37:43 +0900

    0344

---
+++ 2025/leetcode/0344.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        swap = s[::-1]
+        for i in range(len(s)):
+            s[i] = swap[i]
+        
