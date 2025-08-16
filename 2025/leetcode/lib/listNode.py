d70567b6b9eac0ab8cc92f337fb619814347bbe7
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-08-16 22:24:10 +0900

    listNode関数追加

--- 2025/leetcode/lib/listNode.py
+++ 2025/leetcode/lib/listNode.py
@@ -3,8 +3,17 @@ class ListNode:
         self.val = val
         self.next = next
     
+    # ListをListNodeにする
     def listToListNode(self, values: List[int]):
         if values is None or len(values) == 0:
             return None
         return ListNode(val=values[0],next=self.listToListNode(values=values[1:]))
-    
+
+    # ListNodeをListにする
+    def listNodeToList(self, head: Optional[ListNode]):
+        if head is None:
+            return []
+        ans=[head.val]
+        l=self.listNodeToList(head.next)
+        ans.extend(l)
+        return ans
