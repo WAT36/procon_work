e14d1543ae569dd2c19ec793246d609b5211a663
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-11-16 01:19:42 +0900

    0836

---
+++ 2025/leetcode/0836.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+        if (rec1[0] < rec2[0] and rec2[0] < rec1[2]) and (rec2[1] < rec1[3] and rec1[1] < rec2[3]):
+            return True       
+        if (rec1[0] < rec2[2] and rec2[2] < rec1[2]) and (rec2[1] < rec1[3] and rec1[1] < rec2[3]):
+            return True       
+
+        if (rec2[0] < rec1[0] and rec1[0] < rec2[2]) and (rec1[1] < rec2[3] and rec2[1] < rec1[3]):
+            return True       
+        if (rec2[0] < rec1[2] and rec1[2] < rec2[2]) and (rec1[1] < rec2[3] and rec2[1] < rec1[3]):
+            return True          
+       
+        if rec1 == rec2:
+            return True
+        return False
