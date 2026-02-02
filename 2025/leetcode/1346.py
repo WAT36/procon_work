265e7adb50eb21a4da6a56c19bec69bc664fdf91
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2026-02-02 22:32:17 +0900

    1346  ４年半ぶりの出社初日後

---
+++ 2025/leetcode/1346.py
@@ -0,0 +1,6 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        for i in range(len(arr)):
+            if 2*arr[i] in arr and arr.index(2*arr[i]) != i:
+                return True
+        return False
