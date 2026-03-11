960385698b3c29bce719dad79792735943a8b1d8
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2026-03-11 21:32:05 +0900

    1517  mysqlのregexpなんて初めて知った

---
+++ 2025/leetcode/1517.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT
+    user_id,
+    name,
+    mail
+FROM Users
+WHERE REGEXP_LIKE(
+    mail,
+    '^[A-Za-z][A-Za-z0-9_.-]*@leetcode\\.com$',
+    'c'
+);
