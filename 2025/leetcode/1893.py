987bf6a114a7eff9a15373f6ea48820a61759cb2
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2026-06-10 00:04:49 +0900

    1893 梅雨入りかね

---
+++ 2025/leetcode/1893.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        for i in range(left,right+1):
+            for r in ranges:
+                if r[0]<=i and i<=r[1]:
+                    break
+            else:
+                return False
+        return True
