7025046dfc515bc26536e11c48258f84443e6083
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2026-07-01 22:16:18 +0900

    1967 もう７月かえ　出社諦め太郎

---
+++ 2025/leetcode/1967.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numOfStrings(self, patterns: List[str], word: str) -> int:
+        ans=0
+        for p in patterns:
+            if p in word:
+                ans+=1
+        return ans
