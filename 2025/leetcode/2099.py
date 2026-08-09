6061bfabc1cbd877111408a39a7996599147e844
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2026-08-09 22:32:50 +0900

    2099@otofuke   旅行なのに快活・フェリー・快活寝でやっとまともなホテル漕ぎ着けた　のでやった

---
+++ 2025/leetcode/2099.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
+        ansi=sorted(nums)[-1*k:]
+        ans=[]
+        for i in range(len(nums)):
+            if nums[i] in ansi:
+                ans.append(nums[i])
+                ansi.remove(nums[i])
+        return anss
