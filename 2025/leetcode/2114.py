3b13b41e55b9e48649eee1a042823ee9d95bb782
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2026-08-12 19:57:59 +0900

    2114@shibetsu  女満別・遠軽・丸瀬布・留萌・小平・朱鞠内・士別

---
+++ 2025/leetcode/2114.py
@@ -0,0 +1,6 @@
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        ans=0
+        for s in sentences:
+            ans=max(ans,len(s.split(' ')))
+        return ans
